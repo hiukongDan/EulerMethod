f3a2dc912ebdec0ef3af72c968d53e1709353185
hiukongDan <ifsoboy9148@hotmail.com>
2021-02-10 19:39:00 +0800

    fix some display things

--- EulerMethod.py
+++ EulerMethod.py
@@ -6,9 +6,9 @@ class EulerMethod:
         self.init = [initX, initY]
         
     def plot(self, stop, step):
-        dot = self.init
+        dot = [self.init[0], self.init[1]]
         while dot[0] <= stop:
-            print("(%d,%d)"%(dot[0],dot[1]))
+            print("(%f,%f)"%(dot[0],dot[1]))
             slope = self.func(dot[0], dot[1])
             dot[1] = dot[1] + slope * step
             dot[0] += step
@@ -17,4 +17,9 @@ class EulerMethod:
 if __name__ == '__main__':
     print("Python Script: Euler's Method\nUseage: calculating value of given differential equation using euler's method")
     
-    
+    print("Example: dy/dx = 6*x*x - 3*x*x*y with f(0) = 3")
+    M = EulerMethod(lambda x, y: 6*x*x-3*x*x*y, 0,3)
+    print("step = 1")
+    M.plot(1,1)
+    print("step = 0.1")
+    M.plot(1, 0.1)
